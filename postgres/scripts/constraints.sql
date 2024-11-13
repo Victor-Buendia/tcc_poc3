102c68bb02cf73f76bb71bd0623741d9b1efd935
Victor Buendia <victorbuendia03@gmail.com>
2024-11-13 20:11:15 -0300

    🐛 Fix constraint for disciplinas table

--- postgres/scripts/constraints.sql
+++ postgres/scripts/constraints.sql
@@ -5,7 +5,7 @@ ALTER TABLE docentes ADD CONSTRAINT id_docente_pk PRIMARY KEY (matricula);
 ALTER TABLE discentes ADD CONSTRAINT id_discente_pk PRIMARY KEY (matricula);
 ALTER TABLE matriculadisciplinas ADD CONSTRAINT id_matriculadisciplina_pk PRIMARY KEY (id_matricula);
 
-ALTER TABLE disciplinas ADD CONSTRAINT matricula__docentes_to_disciplinas_fk FOREIGN KEY (matricula) REFERENCES disciplinas(matricula_docente);
+ALTER TABLE disciplinas ADD CONSTRAINT matricula_docente__disciplinas_to_docentes_fk FOREIGN KEY (matricula_docente) REFERENCES docentes(matricula);
 ALTER TABLE matriculadisciplinas ADD CONSTRAINT id_disciplina__matriculadisciplinas_to_disciplinas_fk FOREIGN KEY (id_disciplina) REFERENCES disciplinas(id_disciplina);
 ALTER TABLE matriculadisciplinas ADD CONSTRAINT matricula_docente__matriculadisciplinas_to_docentes_fk FOREIGN KEY (matricula_docente) REFERENCES docentes(matricula);
 ALTER TABLE matriculadisciplinas ADD CONSTRAINT matricula_discente__matriculadisciplinas_to_discentes_fk FOREIGN KEY (matricula_discente) REFERENCES discentes(matricula);
